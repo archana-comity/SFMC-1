beca75496428018f94e89268a63d1729ecf839af
archana-comity <archana@comitydesigns.com>
2020-03-10 10:43:17 -0700

    Update customActivity.js

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -55,8 +55,8 @@ define([
         $('#sendWhatsapMsg').click(function() {
            const accountSid = 'ACa24e0798bb0996d1b1cd24786b42c6d8';
             const authToken = '632b8779e6e54a8922d13619507f5b7a';
-            const client = require('twilio')(accountSid, authToken);
-
+            //const client = require('twilio')(accountSid, authToken);
+            const client = Twilio.init(accountSid, authToken);
 
             client.messages
                   .create({
